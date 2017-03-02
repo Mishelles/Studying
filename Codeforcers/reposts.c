2fd868db00eb9cfa77f065606f8c12fc00f53071
Mishelles <sergeenkov.michael@gmail.com>
2017-03-03 01:15:32 +0300

    Working version of code

--- Codeforcers/reposts.c
+++ Codeforcers/reposts.c
@@ -11,31 +11,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #define NAME_LEN 24
 #define BUF_SIZE 60
 
-typedef struct Repost Repost;
 struct Repost{
   char reposter[NAME_LEN];
   char author[NAME_LEN];
+  struct Repost* parent;
 };
-typedef struct List List;
-struct List{
-  char name[NAME_LEN];
-  List *next;
-};
+typedef struct Repost Repost;
 
-int counter(List *head);
-List *init(char name[NAME_LEN]);
-void push(List* head, char name[NAME_LEN]);
-void printList(List* head);
+int counter(Repost *leaf);
+int strComparator(char str1[NAME_LEN], char str2[NAME_LEN]);
+void upCase(char str[NAME_LEN]);
 
 int main(int argc, char const *argv[]) {
   int n; /*name of reposts*/
+  int max_repost_chain=0;
+  int k;
   scanf("%d\n", &n);
   char buf_str[BUF_SIZE];
   char *token; /*will be used in strtok*/
   Repost *reposts_array=(Repost*)malloc(n*sizeof(Repost)); /*In this array we will store all reposts*/
+
+  /*Create array*/
   for (int i = 0; i < n; i++) {
     fgets(buf_str, BUF_SIZE, stdin);
     token=strtok(buf_str, " ");
@@ -43,66 +43,68 @@ int main(int argc, char const *argv[]) {
     token=strtok(NULL, " ");
     token=strtok(NULL, " ");
     strncpy(reposts_array[i].author, token, NAME_LEN);
+    reposts_array[i].parent=NULL;
+    upCase(reposts_array[i].reposter);
+    upCase(reposts_array[i].author);
     memset(buf_str, 0, BUF_SIZE);
     memset(token, 0, strlen(token));
   }
-  /*Debug*/
+
+  /*Create tree*/
   for (int i = 0; i < n; i++){
-    printf("Struct %i:\n", i);
-    printf("reposter:%s\n", reposts_array[i].reposter);
-    printf("author:%s", reposts_array[i].author);
-    printf("------------------------\n");
+    if(strComparator("Polycarp", reposts_array[i].author)==0){
+      for (int j = 0; j < n; j++){
+        if (strComparator(reposts_array[i].author, reposts_array[j].reposter)==1){
+          reposts_array[i].parent=&reposts_array[j];
+          break;
+        }
+      }
+    }
   }
-  List *head=init("Polycarp");
-  for (int i = 0; i < n; i++) {
-    push(head, reposts_array[i].reposter);
+  /*tree traversal*/
+  for (int i = 0; i < n; i++){
+    k=counter(&reposts_array[i]);
+    if(k>max_repost_chain){
+      max_repost_chain=k;
+    }
   }
-  printList(head);
-  free(head);
+  max_repost_chain+=2;
+  printf("%d\n", max_repost_chain);
   free(reposts_array);
   return 0;
 }
 
-int counter(List *head){
+int counter(Repost *leaf){
+  Repost *ptr=leaf;
   int k=0;
-  List* ptr=head;
-  while(ptr->next!=NULL){
+  while(ptr->parent!=NULL){
     ++k;
-    ptr=ptr->next;
+    ptr=ptr->parent;
   }
   return k;
 }
 
-List *init(char name[NAME_LEN]) // а- значение первого узла
-{
-  List *tmp;
-  // выделение памяти под корень списка
-  tmp = (List*)malloc(sizeof(List));
-  strncpy(tmp->name, name, NAME_LEN);
-  tmp->next = NULL; // это последний узел списка
-  return(tmp);
-}
-
-void push(List* head, char name[NAME_LEN])
-{
-  List *tmp, *ptr;
-  tmp = (List*)malloc(sizeof(List));
-  strncpy(tmp->name, name, NAME_LEN);
-  tmp->next=NULL;
-  ptr=head;
-  while(ptr->next!=NULL){
-    ptr=ptr->next;
+int strComparator(char str1[NAME_LEN], char str2[NAME_LEN]){
+  int flag=1;
+  for (int i = 0; i < NAME_LEN; i++) {
+    if ((str1[i]=='\0')||(str2[i]=='\0')){
+      break;
+    }
+    if(str1[i]!=str2[i]){
+      flag=0;
+      break;
+    }
   }
-  ptr->next=tmp;
-  return;
+    return flag;
 }
 
-void printList(List* head)
-{
-  List* ptr=head;
-  while(ptr->next!=NULL){
-    printf("%s\n", ptr->name);
-    ptr=ptr->next;
+void upCase(char str[NAME_LEN]){
+  for (int i = 0; i < NAME_LEN; i++){
+    if(str[i]=='\0'){
+      break;
+    }else{
+      str[i]=toupper(str[i]);
+    }
   }
   return;
 }
