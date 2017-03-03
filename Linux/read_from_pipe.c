7665fd163047c64928f04457dfd80b7bf314afb9
Mishelles <sergeenkov.michael@gmail.com>
2017-03-03 14:10:37 +0300

    add task 1 from module 4 on Stepik

---
+++ Linux/read_from_pipe.c
@@ -0,0 +1,38 @@
+/***********************************************************
+******Linux course on Stepik. Module 4. Task 1.
+-------------------------------------------------------------
+******Program reads another program's output and counts zeros
+******in it uding popen()
+------------------------------------------------------------
+******Created by Mikhail Sergeenkov on the 3rd of March, 2017
+******sergeenkov.michael@gmail.com
+******github.com/Mishelles
+************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LEN 200
+
+int main (int argc, char* const argv[]){
+	if (argc<1){
+		printf("Passing some arguments\n");
+		exit(1);
+	}
+	char command[LEN];
+	int counter=0;
+	sprintf(command, "%s %s", argv[1], argv[2]);
+	FILE *pipe_descr=popen(command, "r");
+	char sym;
+	sym=fgetc(pipe_descr);
+	if (sym=='0') ++counter;
+	while(sym!=EOF){
+		sym=fgetc(pipe_descr);
+		if (sym=='0'){
+			++counter;
+		}
+	}
+	printf("%d\n", counter);
+	pclose(pipe_descr);
+	return 0;
+}
